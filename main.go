3a85871c388cd8fe26e634aaa450cfe798569ae7
benacook <ben.cook@xemteq.com>
2020-05-09 18:46:44 +0100

    add os env. var for port

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/benacook/environment-monitor-go/controllers"
 )
 
 func main() {
+		
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+			port = "3000"
+			log.Printf("Defaulting to port %s", port)
+	}
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+			log.Fatal(err)
+	}
+	// [END setting_port]
+
 	controllers.RegisterControllers()
 	http.ListenAndServe(":3000", nil)
 }
